f594414bf4ecbb8eee06c6e375e12a169ca1393c
Naomi Braun <looseymoose@Naomis-Mistress.local>
2017-03-10 10:08:58 -0500

    greet the world

--- helloworld.py
+++ helloworld.py
@@ -1,19 +1,3 @@
-
 #This is my hello world program
-str1='Hello'
-str2='Tarun'
-print str1 +' '+ str2
 
-# this is my hello world program
-print 'Hello World!'
-
-
-#This is my Hello world program
-str1='Hello'
-str2='Akash'
-print str1 + ' ' + str2 + '!'
-
-#this is a comment
-str1='Hello'
-str2='Priyanka'
-print str1+' '+str2
+print("Hello, world")
